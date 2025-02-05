c8ef4a322ef19fb08c0efe75f340564c745f2138
jjd3109 <jongdoo2001@naver.com>
2025-02-05 18:17:22 +0900

    feat: 좋아요 동시성 문제

--- service/like/src/main/java/micro/board/like/service/LikeService.java
+++ service/like/src/main/java/micro/board/like/service/LikeService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 import kuke.board.common.snowflake.Snowflake;
 import lombok.RequiredArgsConstructor;
+import micro.board.like.entity.ArticleLikeCount;
 import micro.board.like.entity.Like;
+import micro.board.like.repository.ArticleLikeCountRepository;
 import micro.board.like.repository.LikeRepository;
 import micro.board.like.service.request.LikeCreateRequest;
 import micro.board.like.service.response.LikeResponse;
@@ -17,6 +19,7 @@ public class LikeService {
 
 	private final LikeRepository likeRepository;
 	private final Snowflake snowflake = new Snowflake();
+	private final ArticleLikeCountRepository articleLikeCountRepository;
 
 	@Transactional
 	public LikeResponse save(LikeCreateRequest likeCreateRequest) {
@@ -29,6 +32,84 @@ public class LikeService {
 		);
 		return LikeResponse.from(save);
 	}
+
+	/*
+	 * update 구문
+	 */
+	@Transactional
+	public void likePessimisticLock1(LikeCreateRequest likeCreateRequest) {
+		likeRepository.save(
+			Like.create(
+				snowflake.nextId(),
+				likeCreateRequest.getArticleId(),
+				likeCreateRequest.getUserId()
+			)
+		);
+
+		int result = articleLikeCountRepository.increase(likeCreateRequest.getArticleId());
+		if(result == 0 ){
+
+
+			articleLikeCountRepository.save(
+				ArticleLikeCount.init(likeCreateRequest.getArticleId(), 1L)
+			);
+		}
+	}
+
+
+	@Transactional
+	public void unLikePessimisticLock1(Long articleId, Long userId) {
+		likeRepository.findByArticleIdAndUserId(articleId, userId)
+			.ifPresent(articleLike -> {
+				likeRepository.delete(articleLike);
+				articleLikeCountRepository.decrease(articleId);
+			});
+	}
+
+	/*
+	 * update select
+	 */
+	@Transactional
+	public void likePessimisticLock2(LikeCreateRequest likeCreateRequest) {
+		likeRepository.save(
+			Like.create(
+				snowflake.nextId(),
+				likeCreateRequest.getArticleId(),
+				likeCreateRequest.getUserId()
+			)
+		);
+	}
+
+
+	@Transactional
+	public void unLikePessimisticLock2(Long articleId, Long userId) {
+		likeRepository.findByArticleIdAndUserId(articleId, userId)
+			.ifPresent(likeRepository::delete);
+	}
+
+	/*
+	 * 낙관적 락
+	 */
+
+	@Transactional
+	public void likePessimisticLock3(LikeCreateRequest likeCreateRequest) {
+		likeRepository.save(
+			Like.create(
+				snowflake.nextId(),
+				likeCreateRequest.getArticleId(),
+				likeCreateRequest.getUserId()
+			)
+		);
+	}
+
+
+	@Transactional
+	public void unLikePessimisticLock3(Long articleId, Long userId) {
+		likeRepository.findByArticleIdAndUserId(articleId, userId)
+			.ifPresent(likeRepository::delete);
+	}
+
+
 
 
 	public LikeResponse find(Long articleLikeId){
@@ -43,10 +124,5 @@ public class LikeService {
 			.orElseThrow();
 	}
 
-	@Transactional
-	public void delete(Long articleId, Long userId) {
-		likeRepository.findByArticleIdAndUserId(articleId, userId)
-			.ifPresent(likeRepository::delete);
-	}
 
 }

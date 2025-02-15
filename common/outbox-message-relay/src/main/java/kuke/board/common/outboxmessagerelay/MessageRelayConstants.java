375b3606c8cc1e3887679eed44a76bc61f83a82b
jjd3109 <jongdoo2001@naver.com>
2025-02-15 18:06:11 +0900

    feat: Transaction Outbox 모듈 생성

---
+++ common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/MessageRelayConstants.java
@@ -0,0 +1,11 @@
+package kuke.board.common.outboxmessagerelay;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MessageRelayConstants {
+
+	public static final int SHARD_COUNT = 4; // 임의의 샤드 값
+
+}
